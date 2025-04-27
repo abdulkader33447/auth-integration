8124f68d83b3a715719aca18a7435b789934bc90
abdulkader33447 <abdulkader33447@gmail.com>
2025-04-27 19:56:29 +0600

    added login and register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(name,email,password);
+  };
+
   return (
-    <div>
-      <h2>this is register</h2>
+    <div className="card bg-base-100 w-full mx-auto mt-5 max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-3xl font-bold">Register now!</h1>
+        <form onSubmit={handleRegister} className="fieldset">
+          <label className="label">Name</label>
+          <input name="name" type="text" className="input" placeholder="Name" />
+          <label className="label">Email</label>
+          <input
+            name="email"
+            type="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            name="password"
+            type="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+        <p>
+          Already registered? Please{" "}
+          <Link className="text-blue-500 underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
---
+++ src/firebase.init.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// DANGER DONOT SHARE CONFIG IN PUBLIC
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwbrdTiEjU70wXJx2FaBaguKCq9zP6XDQ",
+  authDomain: "auth-integration-dc0fe.firebaseapp.com",
+  projectId: "auth-integration-dc0fe",
+  storageBucket: "auth-integration-dc0fe.firebasestorage.app",
+  messagingSenderId: "792490521056",
+  appId: "1:792490521056:web:240077ee4bc7d35a4e3b85"
+
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
